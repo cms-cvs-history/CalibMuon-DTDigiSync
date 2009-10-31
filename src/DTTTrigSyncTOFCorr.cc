b862be198ea7c9e52cc533c90fc4d4ab1c003f05
cmsbuild <>
2009-10-31 14:53:49 +0000

    This commit was manufactured by cvs2git to create tag 'CalibMuon-DTDigiSync- CMSSW_3_6_0_pre1'.
    Sprout from master 2008-12-11 16:46:41 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Add cout if SL not found in the ttrig map'
    Cherrypick from master 2009-10-31 14:53:15 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Revert to a prior tag and add missing include of Exception.h':
        src/DTTTrigSyncTOFCorr.cc

--- src/DTTTrigSyncTOFCorr.cc
+++ src/DTTTrigSyncTOFCorr.cc
@@ -1,14 +1,15 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2007/02/19 11:45:21 $
- *  $Revision: 1.1 $
+ *  $Date: 2008/11/05 00:02:26 $
+ *  $Revision: 1.2 $
  *  \author G. Cerminara - INFN Torino
  */
 
 #include "DTTTrigSyncTOFCorr.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/Exception.h"
 #include "Geometry/DTGeometry/interface/DTLayer.h"
 #include "Geometry/DTGeometry/interface/DTChamber.h"
 #include "DataFormats/MuonDetId/interface/DTWireId.h"
